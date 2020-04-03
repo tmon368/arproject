20c47de8f800a7908e131adaf32050990f2d7797
prasertpengkaew <30746590+prasertpengkaew@users.noreply.github.com>
2020-04-03 10:41:54 +0700

    create function promotions

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -8,7 +8,60 @@ class Api extends CI_Controller {
 	}
 	public function get_promotions_all(){
 		$this->load->model('ApiModel');
-	    $data = $this->ApiModel->get_pro_all();
-	    echo json_encode($data);
+		$data = $this->ApiModel->get_pro_all();
+		
+	    echo json_encode($data,JSON_UNESCAPED_UNICODE);
+	}
+
+	public function get_promotions_of_tourists()
+	{
+		$this->load->model('ApiModel');
+		$touris_id = $this->uri->segment(3);
+		$data = $this->ApiModel->get_pro_of_tourID($touris_id);
+		echo json_encode($data,JSON_UNESCAPED_UNICODE);
+	}
+
+	public function get_promotions_of_area()
+	{
+		$this->load->model('ApiModel');
+		$lat = $this->uri->segment(3);
+		$long = $this->uri->segment(4);
+		$data = $this->ApiModel->get_promotions_of_area($lat,$long);
+		echo json_encode($data,JSON_UNESCAPED_UNICODE);
+	}
+	public function get_informations()
+	{
+		$this->load->model('ApiModel');
+		$data = $this->ApiModel->get_info_all();
+		
+	    echo json_encode($data,JSON_UNESCAPED_UNICODE);
+	}
+
+	public function tourists_checkIn()
+	{
+		$this->load->model('ApiModel');
+		$touris_id = $this->uri->segment(3);
+		$info_id = $this->uri->segment(4);
+		
+		$promotion = $this->uri->segment(5);
+	}
+
+	public function register_facebook()
+	{
+		$this->load->model('ApiModel');
+		//$email = $this->uri->segment(3);
+		//$name = $this->uri->segment(4);
+		$email = $this->input->get('email');
+		$name = $this->input->get('name');
+		//echo $email,$name;
+		$create_new_user = $this->ApiModel->register_fb($email,$name);
+		echo json_encode($create_new_user,JSON_UNESCAPED_UNICODE);
+	}
+
+	public function login_facebook()
+	{
+		$this->load->model('ApiModel');
+		$email = $this->input->get('email');
+		echo json_encode($data,JSON_UNESCAPED_UNICODE);
 	}
 }
--- application/models/ApiModel.php
+++ application/models/ApiModel.php
@@ -13,13 +13,67 @@ class ApiModel extends CI_Model {
         $this->db->where('job_id',$gameID);
         $query = $this->db->get('bingo_value');
         return $query->result_array();
-        */
+        
     }
     
+    public function get_pro_of_tourID($tourID)
+    {
+        $this ->db->select('PRO_ID')->from('GET_Promotions')->where($tourID);
+        $proid = $this->db->get();
+        if($proid->num_rows()>0)
+        {
+            $this->db->havingIn('Promotions',$proid);
+            $promotion =$this->db->get();
+            return $promotion->result_array();
+        }
+    }
     
+    public function get_pro_of_area($lat,$long)
+    {
+
+    }
+
+    public function get_info_all()
+    {
+        $query = $this->db->get('Informations');
+        return $query->result_array();
+    }
+
+    public function register_fb($email,$name)
+    {
+        if(!empty($email))
+        {
+           
+        $this ->db->select('TOURIS_EMAIL')->from('Tourists')->where(".$email");
+        $fb_email = $this->db->get();
+        if($fb_email->num_rows()==0)
+        {
+            $data_insert =[
+                'TOURIS_NAME'=> $name,
+                'TOURIS_EMAIL'=> $email
+            ];
+            $insertdata = $this->db->table('Tourists')->insert($data_insert);
+            return $insertdata;
+        }
+        else
+        {
+            return "null";
+        }
+    }
+    else{
+        echo "Error Email is null";
+    }
+
+    }
     
     public function getNewSheetID($gameID){
         
+    }
+    public function login_fb($email)
+    {
+        $this->db->from('Tourists')->where('TOURIS_EMAIL',$email);
+        $query = $this->db->get();
+        return $query->result_array();
     }
     
     
